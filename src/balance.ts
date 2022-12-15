9c12ef6f26d03a7b22eb64c7de2169c9a2ae449b
Marlon Castro <msc@ecomp.poli.br>
2022-12-15 17:35:42 -0300

    refactor: chaging the transaction format

--- src/balance.ts
+++ src/balance.ts
@@ -1,88 +1,47 @@
+import 'dotenv/config'
 import Web3 from 'web3'
-import common from 'ethereumjs-common'
-import tx from 'ethereumjs-tx'
+import { AbiItem } from 'web3-utils'
+import { Chain, Common, Hardfork } from '@ethereumjs/common'
+import { Transaction } from '@ethereumjs/tx'
 import MyContractABI from './abis/Test.json'
-
-// configurações para Testnet da Binance 
-const chain = common.default.forCustomChain(
-  'mainnet', {
-  name: 'bnb',
-  networkId: 97,
-  chainId: 97
-},
-  'petersburg'
-)
-
 
 // Mesmo sendo a Binance, o saldo é expresso em ether pois a Binance foi um Hard Fork de Geth com Proof of Stake ao inves de Work
 const init = async () => {
-  const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
+  if(!process.env.URL) return
+  const web3 = new Web3(process.env.URL)
 
   // const id = await web3.eth.net.getId();
-  // Endereço do Contrato na rede 
-  const contractAddress = '0x406FF89AEEdb514c35161440Ab6760b14928F5F1';
+  // Endereço do Contrato a ser executado na rede 
+  if(!process.env.CONTRACT_ADDR) return
+  const contractAddress = process.env.CONTRACT_ADDR;
 
   // Cria uma instância do Contrato 
   const contract = new web3.eth.Contract(
-    MyContractABI,
+    MyContractABI as AbiItem[],
     contractAddress
   )
-
   // Busca se tem alguma conta adicionada ao navegador, ex: Metamask
-  const addresses = await web3.eth.getAccounts();
-  console.log('Lista de endereços de payer: ', addresses);
+  // const addresses = await web3.eth.getAccounts();
+  // console.log('Lista de endereços de payer: ', addresses);
 
   // Conta que executará o contrato (não necessariamente é a mesma que fez o deploy)
   // Contém 0.9929 BNB
-  const account = '0x8d5176A8E81f4cA7B0a160CC08BC0D130640bd60';
-
-  // Chave privada da conta (Necessária caso tenha que fazer transações payable)
-  if(!process.env.PRIVATE_KEY) return
-  const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex');
+  if(!process.env.ACCOUNT) return
+  const account = process.env.ACCOUNT;
 
   // Busca o saldo da conta
   const balanceWei = await web3.eth.getBalance(account)
   const etherBalance = web3.utils.fromWei(balanceWei, 'ether')
-  console.log('Balance: ', etherBalance);
+  console.log('Account Balance: ', etherBalance);
 
   // Executa um Metodo presente no contrato, => Somente leitura / Não altera estado, portanto, não precisa de assinatura e não paga taxas
   const data = await contract.methods.getCount().call();
-  console.log('Antes: ', data);
+  console.log('Count value at Contract: ', data);
 
   // Calculando o gas estimado da operação que altera estado 
-  const contractFunction = contract.methods.count(1);
+  const contractFunction = contract.methods.count(6);
   const estimatedGas = await contractFunction.estimateGas({ from: account })
-  console.log('Estimated gas: ', estimatedGas);
-
-  // Busca o Nonce 
-  const nonce = await web3.eth.getTransactionCount(account)
-  console.log('Nonce: ', nonce.toString(16))
-
-  const txParams = {
-    gasPrice: 10000000000,
-    gasLimit: estimatedGas,
-    to: contractAddress,
-    data: contractFunction.encodeABI(),
-    from: account,
-    nonce: '0x' + nonce.toString(16)
-  };
-  // Cria a Transação com os parametros desejados 
-  const rawTx = new tx.Transaction(txParams, {
-    common: chain
-  });
-
-  // Assina a transação com a chave privada
-  rawTx.sign(privateKey);
-
-  // Envia a transação para alterar o estado no Contrato 
-  web3.eth.sendSignedTransaction('0x' + rawTx.serialize().toString('hex'))
-    .on('receipt', async receipt => {
-      const data = await contract.methods.getCount().call();
-      console.log('depois: ', data);
-    }).catch(err => {
-      console.log(err);
-    })
-
+  console.log('Estimated Gas: ', estimatedGas);
 }
 
 init()
--- src/transact-set.ts
+++ src/transact-set.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import Web3 from 'web3'
 import { AbiItem } from 'web3-utils'
-import { Chain, Common, Hardfork } from '@ethereumjs/common'
+import { Chain, Common, Hardfork, CustomChain } from '@ethereumjs/common'
 import { Transaction } from '@ethereumjs/tx'
 import MyContractABI from './abis/Test.json'
 
@@ -17,11 +17,13 @@ import MyContractABI from './abis/Test.json'
 
 // Mesmo sendo a Binance, o saldo é expresso em ether pois a Binance foi um Hard Fork de Geth com Proof of Stake ao inves de Work
 const init = async () => {
-  const web3 = new Web3('https://api.avax-test.network/ext/bc/C/rpc')
+  if (!process.env.URL) return
+  const web3 = new Web3(process.env.URL)
 
   // const id = await web3.eth.net.getId();
   // Endereço do Contrato a ser executado na rede 
-  const contractAddress = '0x1ffAE3286A101151098ce63bB915Db5B71B48Be7';
+  if (!process.env.CONTRACT_ADDR) return
+  const contractAddress = process.env.CONTRACT_ADDR;
 
   // Cria uma instância do Contrato 
   const contract = new web3.eth.Contract(
@@ -34,18 +36,13 @@ const init = async () => {
 
   // Conta que executará o contrato (não necessariamente é a mesma que fez o deploy)
   // Contém 0.9929 BNB
-  if(!process.env.ACCOUNT) return
+  if (!process.env.ACCOUNT) return
   const account = process.env.ACCOUNT;
 
   // Chave privada da conta (Necessária caso tenha que fazer transações payable)
   if (!process.env.PRIVATE_KEY) return
   const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex');
-
-  // Busca o saldo da conta
-  const balanceWei = await web3.eth.getBalance(account)
-  const etherBalance = web3.utils.fromWei(balanceWei, 'ether')
-  console.log('Balance: ', etherBalance);
-
+  console.log('Private Key: ', privateKey)
   // Executa um Metodo presente no contrato, => Somente leitura / Não altera estado, portanto, não precisa de assinatura e não paga taxas
   const data = await contract.methods.getCount().call();
   console.log('Antes: ', data);
@@ -57,43 +54,82 @@ const init = async () => {
 
   // Busca o Nonce 
   const nonce = await web3.eth.getTransactionCount(account)
+  
+  const common = Common.custom({ chainId: 43113, networkId: 97, name: 'avax'})
 
   const txParams = {
+    from: account,
     nonce: web3.utils.toHex(nonce),
     to: contractAddress,
-    value: web3.utils.toHex(web3.utils.toWei('0', 'ether')),
-    gasPrice: web3.utils.toHex(2100000),
-    gasLimit: web3.utils.toHex(web3.utils.toWei('6', 'gwei')),
+    value: '0x00',
+    gasPrice: web3.utils.toHex(21000),
+    gasLimit: web3.utils.toHex(web3.utils.toWei('10', 'gwei')),
     data: contractFunction.encodeABI(),
-    chainId: web3.utils.toHex(43114)
+    chainId: 43113,
+    common 
   };
 
-  const common = new Common({ chain: Chain.Mainnet, hardfork: Hardfork.Istanbul })
 
-  // Cria a Transação com os parametros desejados 
-  // const rawTx = new tx.Transaction(txParams, {
-  //   common: chain
-  // });
-  
   const tx = Transaction.fromTxData(txParams, { common })
 
   // Assina a transação com a chave privada
-  tx.sign(privateKey);
+  const signedTx = tx.sign(privateKey);
 
-  const serializeTx = tx.serialize().toString('hex')
-  const raw = web3.utils.toHex(serializeTx)
+  const serializeTx = signedTx.serialize()
+
   // Envia a transação para alterar o estado no Contrato 
-  web3.eth.sendSignedTransaction(raw)
+  web3.eth.sendSignedTransaction(serializeTx.toString('hex'))
     .on('receipt', async receipt => {
       const data = await contract.methods.getCount().call();
       console.log('Depois: ', data);
     }).catch(err => {
       console.log('Erro =>>>> ', err);
     })
-
 }
 
 init()
+
+
+
+
+
+{/** 
+
+const Web3 = require('web3')
+const Tx = require('@ethereumjs/tx');
+const Common = require('@ethereumjs/common') 
+
+const common = new Common.default({ chain: 'ropsten' })
+
+const w3 = new Web3('https://ropsten.infura.io/v3/<apikey>')
+
+ const key = '3c9...'
+ const address = '0x3c6E2d83dFAd3858B55C978576b0Ba697C50a43c'
+
+var privateKey = Buffer.from(key, 'hex')
+
+const rawTx = {
+    nonce: w3.utils.toHex(0), //to be incremented for each new transaction with this account
+    from: address,
+    to: '0xe381c25de995d62b453af8b931aac84fccaa7a62',
+    gasLimit: w3.utils.toHex(21000),
+    gasPrice: w3.utils.toHex(10e9), //10 Gwei
+    value: w3.utils.toHex(0),
+    //chainId: w3.utils.toHex(3), optional parameter
+};
+
+let tx = Tx.Transaction.fromTxData(rawTx, {common})
+const signedTx = tx.sign(privateKey);
+//encode the transaction in rlp format
+const serializedTx = signedTx.serialize()
+
+let tx_hash = w3.eth.sendSignedTransaction(w3.utils.toHex(serializedTx))
+  .on('receipt', console.log)
+     .catch(e => console.log('e: ', e));
+
+*/}
+
+
 
 
 
