98159ec196b529df12497b4e4c6c10a841c39462
Marlon Castro <msc@ecomp.poli.br>
2022-12-15 17:48:35 -0300

    fix: revert to initial commit to apply updates

--- avaxContract.js
+++ avaxContract.js
@@ -1,44 +1,41 @@
-import Web3 from 'web3'
-import common from 'ethereumjs-common'
-import tx from 'ethereumjs-tx'
-import MyContractABI from './abis/Test.json'
+const Web3 = require('web3');
+const common = require('ethereumjs-common');
+const tx = require('ethereumjs-tx');
+const MyContractABI = require('./src/abis/Test.json');
 
-// configurações para Testnet da Binance 
+// configurações para Testnet da AVAX 
 const chain = common.default.forCustomChain(
   'mainnet', {
-  name: 'bnb',
+  name: 'avax',
   networkId: 97,
-  chainId: 97
+  chainId: 43113
 },
   'petersburg'
 )
 
-
 // Mesmo sendo a Binance, o saldo é expresso em ether pois a Binance foi um Hard Fork de Geth com Proof of Stake ao inves de Work
 const init = async () => {
-  const web3 = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545')
+  const web3 = new Web3('https://api.avax-test.network/ext/bc/C/rpc')
 
   // const id = await web3.eth.net.getId();
   // Endereço do Contrato na rede 
-  const contractAddress = '0x406FF89AEEdb514c35161440Ab6760b14928F5F1';
+  const contractAddress = '0x1ffAE3286A101151098ce63bB915Db5B71B48Be7';
 
   // Cria uma instância do Contrato 
   const contract = new web3.eth.Contract(
     MyContractABI,
     contractAddress
   )
-
   // Busca se tem alguma conta adicionada ao navegador, ex: Metamask
-  const addresses = await web3.eth.getAccounts();
-  console.log('Lista de endereços de payer: ', addresses);
+  // const addresses = await web3.eth.getAccounts();
+  // console.log('Lista de endereços de payer: ', addresses);
 
   // Conta que executará o contrato (não necessariamente é a mesma que fez o deploy)
   // Contém 0.9929 BNB
   const account = '0x8d5176A8E81f4cA7B0a160CC08BC0D130640bd60';
 
   // Chave privada da conta (Necessária caso tenha que fazer transações payable)
-  if(!process.env.PRIVATE_KEY) return
-  const privateKey = Buffer.from(process.env.PRIVATE_KEY, 'hex');
+  const privateKey = Buffer.from('4fca24389e8280d485756a46e1ddbff02b41626dfdb32048953d65873cdd0f99', 'hex');
 
   // Busca o saldo da conta
   const balanceWei = await web3.eth.getBalance(account)
@@ -50,16 +47,15 @@ const init = async () => {
   console.log('Antes: ', data);
 
   // Calculando o gas estimado da operação que altera estado 
-  const contractFunction = contract.methods.count(1);
+  const contractFunction = contract.methods.count(6);
   const estimatedGas = await contractFunction.estimateGas({ from: account })
-  console.log('Estimated gas: ', estimatedGas);
+  console.log('Gas estimado: ', estimatedGas);
 
   // Busca o Nonce 
   const nonce = await web3.eth.getTransactionCount(account)
-  console.log('Nonce: ', nonce.toString(16))
 
   const txParams = {
-    gasPrice: 10000000000,
+    gasPrice: 25000000000,
     gasLimit: estimatedGas,
     to: contractAddress,
     data: contractFunction.encodeABI(),
@@ -76,12 +72,12 @@ const init = async () => {
 
   // Envia a transação para alterar o estado no Contrato 
   web3.eth.sendSignedTransaction('0x' + rawTx.serialize().toString('hex'))
-    .on('receipt', async receipt => {
-      const data = await contract.methods.getCount().call();
-      console.log('depois: ', data);
-    }).catch(err => {
-      console.log(err);
-    })
+  .on('receipt', async receipt => {
+    const data = await contract.methods.getCount().call();
+    console.log('Depois: ', data);
+  }).catch(err => {
+    console.log(err);
+  })
 
 }
 
